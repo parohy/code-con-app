23feb804612f49385b621535f22d6316b1f77988
Tomas Paronai <tomas.paronai@goodrequest.com>
2022-04-14 15:27:47 +0200

    login UI

---
+++ app/src/main/java/sk/parohy/codecon/LoginActivity.kt
@@ -0,0 +1,9 @@
+package sk.parohy.codecon
+
+import sk.parohy.codecon.databinding.ActivityLoginBinding
+
+class LoginActivity: VBActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
+    override fun ActivityLoginBinding.onBind() {
+
+    }
+}
---
+++ app/src/main/java/sk/parohy/codecon/VBActivity.kt
@@ -0,0 +1,17 @@
+package sk.parohy.codecon
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class VBActivity<VB: ViewBinding>(inflate: (LayoutInflater) -> VB): AppCompatActivity() {
+    private val binding: VB by lazy { inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.onBind()
+    }
+
+    abstract fun VB.onBind()
+}
